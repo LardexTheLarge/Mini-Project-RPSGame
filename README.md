c8f22275ca1591011d4f2738fd50590f0d4236fd
Gabe Vee <0gabevee0@gmail.com>
2022-08-07 18:48:13 -0600

    Finished the code to track stats and added a for loop to play again

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Mini-Project-RPSGame
+
+This Rock, Paper, Scissor game was made only with Javascript and played with confirm, alert and prompt pop ups. It starts of with picking your move R, P or S (Rock, Paper, Scissor). Then the computer picks their move and if you win then your stats get track as wins, losses or ties.
--- assets/Js/script.js
+++ assets/Js/script.js
@@ -4,30 +4,63 @@ var loss = 0;
 var ties = 0;
 var computerChoices = ["R", "P", "S"];
 
-//user choice
-var userMove = prompt("enter R, P or S").toUpperCase();
-console.log(userMove);
+function playRPS() {
+  //user choice
+  var userMove = prompt(
+    "Enter R for Rock, P for Paper or S for Scissors:"
+  ).toUpperCase();
+  console.log(userMove);
 
-//PC choice
-var pcMove =
-  computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  //computer choice
+  var pcMove =
+    computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  console.log(pcMove);
 
-var showMove = confirm("pc chose " + pcMove);
-console.log(showMove);
+  var showMove = confirm("The Computer choose " + pcMove);
 
-//Confirms to User that they won or Lost
-if (userMove === "R" && pcMove === "P") {
-  var pcWin = confirm("You lose");
-} else if (userMove === "S" && pcMove === "P") {
-  var userWin = confirm("You win");
-} else if (userMove === "P" && pcMove === "R") {
-  var userWin = confirm("You win");
-} else if (userMove === "R" && pcMove === "S") {
-  var userWin = confirm("You win");
-} else if (userMove === "P" && pcMove === "S") {
-  var pcWin = confirm("You lose");
-} else if (userMove === "S" && pcMove === "R") {
-  var pcWin = confirm("You Lose");
-} else {
-  var tie = confirm("this was a tie");
+  //Confirms to User that they won or Lost
+  if (userMove === "R" && pcMove === "P") {
+    var pcWin = confirm("You lose");
+  } else if (userMove === "S" && pcMove === "P") {
+    var userWin = confirm("You win");
+  } else if (userMove === "P" && pcMove === "R") {
+    var userWin = confirm("You win");
+  } else if (userMove === "R" && pcMove === "S") {
+    var userWin = confirm("You win");
+  } else if (userMove === "P" && pcMove === "S") {
+    var pcWin = confirm("You lose");
+  } else if (userMove === "S" && pcMove === "R") {
+    var pcWin = confirm("You Lose");
+  } else {
+    var tie = confirm("this was a tie");
+  }
+
+  //logic to show the stats of the user
+  if (userWin === true) {
+    win++;
+  } else if (pcWin === true) {
+    loss++;
+  } else {
+    ties++;
+  }
+  console.log("wins " + win);
+  console.log("Losses " + loss);
+  console.log("Ties " + ties);
+
+  //Users stats show up as an alert
+  var stats = confirm(
+    "User's Stats:\nwins: " + win + "\nLosses: " + loss + "\nTies: " + ties
+  );
+
+  //This if else statement continues the game
+  var restart = "play again";
+  if (restart === true) {
+    for (i = 0; i < playRPS(); i++) {
+      playAgain();
+    }
+  } else {
+    alert("Restarting Game(If you want to quit restart webpage)");
+    window.location.reload(playRPS());
+  }
 }
+playRPS();
